034ec1ff7b7a80b326082b1f704745454317525e
BlackTail2000 <carlosgabrielbacamanrique@hotmail.com>
2024-09-27 18:59:35 -0500

    User1 y User2 Creados

---
+++ src/main/java/pe/edu/cibertec/app_reqres_client/client/placeholder/model/User1.java
@@ -0,0 +1,12 @@
+package pe.edu.cibertec.app_reqres_client.client.placeholder.model;
+
+import lombok.Data;
+
+@Data
+public class User1 {
+    private Long id;
+    private String email;
+    private String first_name;
+    private String last_name;
+    private String avatar;
+}
