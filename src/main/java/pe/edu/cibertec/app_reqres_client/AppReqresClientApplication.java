67ebd34bb57ca8ca3ee3f2b891c06d0f8a859d4e
BlackTail2000 <carlosgabrielbacamanrique@hotmail.com>
2024-09-27 18:44:39 -0500

    Proyecto Creado

---
+++ src/main/java/pe/edu/cibertec/app_reqres_client/AppReqresClientApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.app_reqres_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppReqresClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppReqresClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/app_reqres_client/AppReqresClientApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.app_reqres_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppReqresClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
