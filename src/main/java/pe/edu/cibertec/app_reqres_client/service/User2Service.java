4fdf99703f9969476a985b17085a4b5a0d621ead
BlackTail2000 <carlosgabrielbacamanrique@hotmail.com>
2024-09-27 19:30:48 -0500

    User2 Service Client Terminado

---
+++ src/main/java/pe/edu/cibertec/app_reqres_client/service/User2Service.java
@@ -0,0 +1,16 @@
+package pe.edu.cibertec.app_reqres_client.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import pe.edu.cibertec.app_reqres_client.client.placeholder.iclientservice.User2Client;
+import pe.edu.cibertec.app_reqres_client.client.placeholder.model.User2;
+
+@Service
+@RequiredArgsConstructor
+public class User2Service {
+    private final User2Client user2Client;
+
+    public User2 createUser(User2 user) {
+        return user2Client.createUser(user);
+    }
+}
